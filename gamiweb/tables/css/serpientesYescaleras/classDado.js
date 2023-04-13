2cab441312ddb37f80f128644675cfcb0e01271f
karenHuff <53838303+karenHuff@users.noreply.github.com>
2023-04-13 17:17:31 -0600

    Add files via upload

---
+++ gamiweb/tables/css/serpientesYescaleras/classDado.js
@@ -0,0 +1,15 @@
+class Dado
+{
+	lanzar()
+	{
+		//Generar numero aleatorio entre 1 y 6
+		var posiciones = Math.floor(Math.random() * 6) + 1;
+		//reiniciar();
+		return posiciones;
+		//escogerPreguntaAleatoria();
+		this.reiniciarFicha();
+
+	}
+
+		
+}
